cb2b35d96049cdece6bbcdc998e349229635a5e4
MeadowsProductions <135252573+MeadowsProductions@users.noreply.github.com>
2023-12-12 10:18:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const $ = e => document.querySelector(e);
+const $$ = e => document.querySelectorAll(e);
+
+const calcButton = $(".calculate");
+const dateInputs = $$("input[type=date]");
+const timeInputs = $$("input[type=text]");
+const perMin = $(".perMin");
+const perDay = $(".perDay");
+const hours = $(".hours");
+const firstMoney = $(".first");
+const secondMoney = $(".second");
+const results = $(".results");
+
+let min;
+let day;
+let firstDate;
+let secondDate;
+let timeResult;
+let tempMoneyVar;
+
+calcButton.addEventListener("click", () => {
+    calculate();
+})
+
+function calculate() {
+    firstDate = new Date(dateInputs[0].value + " " + timeInputs[0].value);
+    secondDate = new Date(dateInputs[1].value + " " + timeInputs[1].value);
+    timeResult = (secondDate - firstDate) / 60000;
+    tempMoneyVar = ((secondMoney.value - firstMoney.value) / timeResult).toFixed(0);
+    perMin.innerText = "Per Minute: " + parseFloat(tempMoneyVar).toLocaleString();
+    perDay.innerText = "Per Day: " + parseFloat(tempMoneyVar * 1440).toLocaleString();
+    hours.innerText = "Total Hours: " + (timeResult / 60).toFixed(0);
+    results.style.opacity = "1";
+}
