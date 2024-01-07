c05ef5478808ad28cec803514fa5d3d3d108e6e8
MeadowsProductions <135252573+MeadowsProductions@users.noreply.github.com>
2024-01-06 20:36:43 -0600

    new feature :O :O :O

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const $$ = e => document.querySelectorAll(e);
 
 // Helpful functions :)
 const parseValue = (input) => parseFloat(input.value.replace(/,/g, ''));
-const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
+const parseNumber = (value) => value.toString().replace(/,/g, '');
 
 // DOM elements
 const calcButton = $(".calculate");
@@ -12,6 +12,7 @@ const dateInputs = $$("input[date]");
 const timeInputs = $$("input[time]");
 const firstFI = $(".firstFI");
 const secondFI = $(".secondFI");
+const fis = [firstFI, secondFI]
 const labelInput = $("input[label]");
 const perMin = $(".perMin");
 const perHour = $(".perHour");
@@ -23,12 +24,26 @@ const results = $(".results");
 const historyDisplay = $(".history");
 const clearHistory = $(".clearHistory");
 const copyDate = $(".copyDate");
+const double = $(".double");
+const fixed = $(".fixed");
 
 // Variables for calculations
 let tempOne, tempTwo, firstDate, secondDate, timeResult, time, dateOne, dateTwo, seconds, minutes, hours, days;
+let doubleMode = false;
 
 // Event listener for the calculate button
 calcButton.addEventListener("click", calculate);
+
+// Event listener for double mode calculations
+double.addEventListener("click", () => {
+    if(doubleMode){
+        doubleMode = false
+        double.style.backgroundColor = "red";
+    } else {
+        doubleMode = true;
+        double.style.backgroundColor = "#00FF00";
+    }
+})
 
 // Event listeners for formatting time inputs
 timeInputs.forEach(el => {
@@ -45,21 +60,18 @@ timeInputs.forEach(el => {
 clearHistory.addEventListener("click", clearLocalStorage);
 
 // Load and display history if available
-let savedHistory = JSON.parse(localStorage.getItem("historyV2")) || [];
+let savedHistory = JSON.parse(localStorage.getItem("historyV3")) || [];
 updateHistory(savedHistory);
 
 // WHAT DO YOU THINK IT DOES
 function calculate() {
-    const moneys = [parseValue(firstMoney), parseValue(secondMoney)];
+    const moneys = [parseValue(firstMoney), parseValue(secondMoney)]; fixed.innerText = "";
     const times = [new Date(dateInputs[0].value + " " + timeInputs[0].value), new Date(dateInputs[1].value + " " + timeInputs[1].value)], time = (times[1] - times[0]) / 60000, moneyResult = (moneys[1] - moneys[0]) / time;
     perMin.innerText = "Per Minute: " + parseFloat(moneyResult.toFixed(0)).toLocaleString(), perHour.innerText = "Per Hour: " + parseFloat((moneyResult * 60).toFixed(0)).toLocaleString(), perDay.innerText = "Per Day: " + parseFloat((moneyResult * 1440).toFixed(0)).toLocaleString(); results.style.opacity = "1";
-    seconds = ((times[1] - times[0]) / 1000).toFixed(0), minutes = 0, hours = 0, days = 0;
-    while (seconds >= 60) { seconds -= 60; minutes++ }
-    while (minutes >= 60) { minutes -= 60; hours++ }
-    while (hours >= 24) { hours -= 24; days++ }
-    seconds = seconds.toString().padStart(2, "0"), minutes = minutes.toString().padStart(2, "0"), hours = hours.toString().padStart(2, "0"), days = days.toString().padStart(2, "0");
-    timeElapsed.innerText = `Time Elapsed: ${days}:${hours}:${minutes}:${seconds}`;
-    addHistory(labelInput.value, Math.floor(moneyResult));
+    let elapsedTime = formatTimeTime((times[1] - times[0]) / 1000) 
+    timeElapsed.innerText = `Time Elapsed: ${elapsedTime}`;
+    addHistory(labelInput.value, Math.floor(moneyResult), ((times[1] - times[0]) / 1000));
+    if(doubleMode){fixed.innerText = "Without 2X: " + parseFloat(Math.floor(moneyResult / 2)).toLocaleString()}
 }
 
 // Function to format time inputs
@@ -74,6 +86,16 @@ function formatMoneyInput() {
     if (this.value) {
         this.value = parseFloat(this.value).toLocaleString();
     }
+}
+
+// :)
+function formatTimeTime(time) {
+    seconds = time, minutes = 0, hours = 0, days = 0;
+    while (seconds >= 60) { seconds -= 60; minutes++ }
+    while (minutes >= 60) { minutes -= 60; hours++ }
+    while (hours >= 24) { hours -= 24; days++ }
+    seconds = seconds.toString().padStart(2, "0"), minutes = minutes.toString().padStart(2, "0"), hours = hours.toString().padStart(2, "0"), days = days.toString().padStart(2, "0");
+    return `${days}:${hours}:${minutes}:${seconds}`;
 }
 
 // Function to remove formatting on focus
@@ -96,61 +118,41 @@ function updateHistory(history) {
     for (let i = 0; i < history.length; i++) {
         const entry = history[i];
         const entryElement = document.createElement("div");
-        entryElement.innerHTML = `<h2>${entry.label}:</h2><h2>${entry.result}</h2>`;
+        entryElement.innerHTML = `<h2 class="pointer">${entry.label}:</h2><h2 class="pointer">${entry.result}</h2>`;
         historyDisplay.appendChild(entryElement);
+        entryElement.addEventListener("click", () => {
+            let money = parseNumber(entry.result);
+            let time = formatTimeTime(entry.time);
+            results.style.opacity = "1";
+            perMin.innerText = "Per Minute: " + parseFloat(money).toLocaleString();
+            perHour.innerText = "Per Hour: " + parseFloat(money * 60).toLocaleString();
+            perDay.innerText = "Per Day: " + parseFloat(money * 1440).toLocaleString();
+            timeElapsed.innerText = "Time Elapsed: " + time;
+        })
     }
 }
 
 // Function to add history
-function addHistory(label, money) {
-    const resultEntry = { label: label || "[Blank]", result: parseFloat(money).toLocaleString() };
+function addHistory(label, money, time) {
+    if(doubleMode) {money = money / 2}
+    const resultEntry = { label: label || "[Blank]", result: parseFloat(money).toLocaleString(), time: time };
     savedHistory.push(resultEntry);
-    localStorage.setItem("historyV2", JSON.stringify(savedHistory));
+    localStorage.setItem("historyV3", JSON.stringify(savedHistory));
     updateHistory(savedHistory);
 }
 
 // Function to copy date
-copyDate.addEventListener("click", () => {
-    if (dateInputs[1].value === "") {
-        dateInputs[1].value = dateInputs[0].value;
+copyDate.addEventListener("click", () => {if (dateInputs[1].value === "") {dateInputs[1].value = dateInputs[0].value;} else {dateInputs[0].value = dateInputs[1].value;}})
+
+// Function to insert files (regex is crazy)
+fis.forEach((el, i) => el.addEventListener("change", () => {
+    const fileName = el.files[0].name;
+    if (fileName && fileName.toLowerCase().includes("robloxscreenshot")) {
+        let date = fileName.slice(16); date = date.slice(0, 4) + "-" + date.slice(4, 6) + "-" + date.slice(6);
+        let time = date.slice(11); time = time.slice(0, 2) + ":" + time.slice(2, 4) + ":" + time.slice(4, 6);
+        dateInputs[i].value = date.slice(0, 10);
+        timeInputs[i].value = time;
     } else {
-        dateInputs[0].value = dateInputs[1].value;
+        alert("Please use Roblox screenshots."); el.value = null;
     }
-})
-
-// Function to insert files (holyt sh)
-firstFI.addEventListener("change", () => {
-    if (firstFI.files.length > 0) {
-        const fileName = firstFI.files[0].name;
-        if (fileName.toLowerCase().includes("robloxscreenshot")) {
-            let date = fileName.slice(16);
-            date = date.slice(0, 4) + "-" + date.slice(4, 6) + "-" + date.slice(6);
-            let time = date.slice(11);
-            time = time.slice(0, 2) + ":" + time.slice(2, 4) + ":" + time.slice(4, 6);
-            dateInputs[0].value = date.slice(0, 10);
-            timeInputs[0].value = time;
-        } else {
-            alert("Please use roblox screen shots.");
-            firstFI.value = null;
-        }
-    }
-})
-
-// Second file input
-secondFI.addEventListener("change", () => {
-    if (secondFI.files.length > 0) {
-        const fileName = secondFI.files[0].name;
-        if (fileName.toLowerCase().includes("robloxscreenshot")) {
-            let date = fileName.slice(16);
-            date = date.slice(0, 4) + "-" + date.slice(4, 6) + "-" + date.slice(6);
-            let time = date.slice(11);
-            time = time.slice(0, 2) + ":" + time.slice(2, 4) + ":" + time.slice(4, 6);
-            dateInputs[1].value = date.slice(0, 10);
-            timeInputs[1].value = time;
-        } else {
-            alert("Please use roblox screen shots.");
-            secondFI.value = null;
-        }
-    }
-})
-
+}));
